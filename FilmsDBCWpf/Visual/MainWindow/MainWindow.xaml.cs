e39062ddab19efed646986775672ff7a508f1d85
Andrey Le <sirkoandrew84@gmail.com>
2021-10-27 00:41:17 +0300

    Внёс первые изменения

--- FilmsDBCWpf/Visual/MainWindow/MainWindow.xaml.cs
+++ FilmsDBCWpf/Visual/MainWindow/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FilmsDBCWpf
+namespace FilmsDBCWpf.Visual.MainWindow
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainForm : Window
     {
-        public MainWindow()
+        public MainForm()
         {
             InitializeComponent();
         }
---
+++ FilmsDBCWpf/Visual/StaticVisualClasses/VisualHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmsDBCWpf.Visual.StaticVisualClasses
+{
+    class VisualHelper
+    {
+		public static String markToText(String mark)
+		{
+			switch (mark)
+			{
+				case "":
+					return mark;
+
+				case "6":
+					return mark + "/6";
+
+				default:
+					return mark + "/5";
+			}
+		}
+
+		public static int TextToInt32(String str)
+		{
+			if (str == "")
+			{
+				return 0;
+			}
+			else
+			{
+				return Convert.ToInt32(str);
+			}
+		}
+
+		public static sbyte TextToMark(String str)
+		{
+			if (str == "")
+			{
+				return -1;
+			}
+			else
+			{
+				return Convert.ToSByte(str.Substring(0, str.IndexOf('/')));
+			}
+		}
+	}
+}

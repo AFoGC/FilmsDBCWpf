5fdbf2f8009b5fe9c9beadbb25d954d7e822f53c
Олександр Гончаренко <sasha13062003@gmail.com>
2021-11-11 18:01:35 +0200

    Трьохуровневая архитектура

---
+++ DAL_Films/BaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_Films
+{
+    public class BaseConnection
+    {
+        private string connectionString;
+        public SqlConnection connection;
+        public SqlCommand command;
+        public BaseConnection()
+        {
+            connectionString = ConfigurationManager.
+                ConnectionStrings["ConnectionString"].ConnectionString;
+
+            connection = new SqlConnection(connectionString);
+
+            command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+
+            openConnection();
+        }
+        private SqlConnection openConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State == ConnectionState.Broken)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+    }
+}

ab9ffe0c329fc7aeff9af8316847d1fa7881cd3e
Andrey Le <sirkoandrew84@gmail.com>
2022-01-04 22:13:35 +0200

    Испарвил элементы на readonly и исправил баги

--- WpfApp/Visual/MainWindow/GlobalElements/Menus/FilmsMenu/UpdateControls/FilmUpdateControl.xaml.cs
+++ WpfApp/Visual/MainWindow/GlobalElements/Menus/FilmsMenu/UpdateControls/FilmUpdateControl.xaml.cs
@@ -18,99 +18,99 @@ using WpfApp.Visual.StaticVisualClasses;
 
 namespace WpfApp.Visual.MainWindow.GlobalElements.Menus.FilmsMenu.UpdateControls
 {
-    /// <summary>
-    /// Логика взаимодействия для FilmUpdateControl.xaml
-    /// </summary>
-    public partial class FilmUpdateControl : UserControl, IUpdateControl
-    {
-        private FilmControl filmControl = null;
-        private Film film = null;
-        public FilmUpdateControl(FilmControl filmControl)
-        {
-            InitializeComponent();
-            this.filmControl = filmControl;
-            this.film = filmControl.FilmInfo;
+	/// <summary>
+	/// Логика взаимодействия для FilmUpdateControl.xaml
+	/// </summary>
+	public partial class FilmUpdateControl : UserControl, IUpdateControl
+	{
+		private FilmControl filmControl = null;
+		private Film film = null;
+		public FilmUpdateControl(FilmControl filmControl)
+		{
+			InitializeComponent();
+			this.filmControl = filmControl;
+			this.film = filmControl.FilmInfo;
 
-            foreach (var item in MainInfo.Tables.GenresTable)
-            {
-                genre.Items.Add(item);
-            }
-            foreach (var item in FilmMethods.GetAllMarks().ToArray())
-            {
-                mark.Items.Add(item);
-            }
+			foreach (var item in MainInfo.Tables.GenresTable)
+			{
+				genre.Items.Add(item);
+			}
+			foreach (var item in FilmMethods.GetAllMarks().ToArray())
+			{
+				mark.Items.Add(item);
+			}
 
-            refresh();
-        }
+			refresh();
+		}
 
-        private void refresh()
-        {
-            Film defFilm = MainInfo.Tables.FilmsTable.DefaultCell;
+		private void refresh()
+		{
+			Film defFilm = MainInfo.Tables.FilmsTable.DefaultCell;
 
-            this.id.Text = film.ID.ToString();
-            this.name.Text = film.Name;
-            this.genre.SelectedItem = film.Genre;
-            this.realiseYear.Text = Film.FormatToString(film.RealiseYear, defFilm.RealiseYear);
-            this.watched.IsChecked = film.Watched;
-            this.mark.SelectedItem = VisualHelper.markToText(Film.FormatToString(film.Mark, defFilm.Mark));
-            this.countOfViews.Text = Film.FormatToString(film.CountOfViews, defFilm.CountOfViews);
-            this.watchDate.Date = film.DateOfWatch;
-            this.comment.Text = film.Comment;
-        }
+			this.id.Text = film.ID.ToString();
+			this.name.Text = film.Name;
+			this.genre.SelectedItem = film.Genre;
+			this.realiseYear.Text = Film.FormatToString(film.RealiseYear, defFilm.RealiseYear);
+			this.watched.IsChecked = film.Watched;
+			this.mark.SelectedItem = VisualHelper.markToText(Film.FormatToString(film.Mark, defFilm.Mark));
+			this.countOfViews.Text = Film.FormatToString(film.CountOfViews, defFilm.CountOfViews);
+			this.watchDate.Date = film.DateOfWatch;
+			this.comment.Text = film.Comment;
+		}
 
-        public void UpdateElement()
-        {
-            film.Name = this.name.Text;
-            if (!film.Genre.IsSerialGenre && ((Genre)this.genre.SelectedItem).IsSerialGenre)
-            {
-                MainInfo.Tables.SeriesTable.FindAndConnectSerie(film);
-            }
-            film.Genre = (Genre)this.genre.SelectedItem;
-            film.RealiseYear = VisualHelper.TextToInt32(this.realiseYear.Text);
-            film.Watched = (bool)this.watched.IsChecked;
-            film.Mark = VisualHelper.TextToMark(this.mark.Text);
-            film.CountOfViews = VisualHelper.TextToInt32(this.countOfViews.Text);
-            film.DateOfWatch = this.watchDate.Date;
-            film.Comment = this.comment.Text;
+		public void UpdateElement()
+		{
+			film.Name = this.name.Text;
+			if (!film.Genre.IsSerialGenre && ((Genre)this.genre.SelectedItem).IsSerialGenre)
+			{
+				MainInfo.Tables.SeriesTable.FindAndConnectSerie(film);
+			}
+			film.Genre = (Genre)this.genre.SelectedItem;
+			film.RealiseYear = VisualHelper.TextToInt32(this.realiseYear.Text);
+			film.Watched = (bool)this.watched.IsChecked;
+			film.Mark = VisualHelper.TextToMark(this.mark.Text);
+			film.CountOfViews = VisualHelper.TextToInt32(this.countOfViews.Text);
+			film.DateOfWatch = this.watchDate.Date;
+			film.Comment = this.comment.Text;
 
-            filmControl.RefreshData();
-        }
+			filmControl.RefreshData();
+		}
 
-        private void btn_sources_Click(object sender, RoutedEventArgs e)
-        {
-            MainInfo.MainWindow.FilmsMenu.UpdateVisualizer.SourcesVisualizer.OpenSourceControl(this.film.Sources);
-        }
+		private void btn_sources_Click(object sender, RoutedEventArgs e)
+		{
+			MainInfo.MainWindow.FilmsMenu.UpdateVisualizer.SourcesVisualizer.OpenSourceControl(this.film.Sources);
+		}
 
-        private bool commentIsOpen = false;
-        private void btn_comment_Click(object sender, RoutedEventArgs e)
-        {
-            if (commentIsOpen) { this.grid.Height -= 20; }
-            else { this.grid.Height += 20; }
+		private bool commentIsOpen = false;
+		private void btn_comment_Click(object sender, RoutedEventArgs e)
+		{
+			if (commentIsOpen) { this.grid.Height -= 20; }
+			else { this.grid.Height += 20; }
 
-            commentIsOpen = !commentIsOpen;
-        }
+			commentIsOpen = !commentIsOpen;
+		}
 
-        private void watched_Click(object sender, RoutedEventArgs e)
-        {
-            if (watchDate.IsEmpty)
-            {
-                watchDate.Date = DateTime.Today;
-            }
+		private void watched_Click(object sender, RoutedEventArgs e)
+		{
+			if (watchDate.IsEmpty)
+			{
+				watchDate.Date = DateTime.Today;
+			}
 
-            if (countOfViews.Text == "")
-            {
-                countOfViews.Text = "1";
-            }
-        }
+			if (countOfViews.Text == "")
+			{
+				countOfViews.Text = "1";
+			}
+		}
 
-        private void genre_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            /*
-            if (((Genre)genre.SelectedItem).IsSerialGenre)
-            {
-                MainInfo.MainWindow.FilmsMenu.UpdateVisualizer.OpenUpdateControl(filmControl);
-            }
-            */
-        }
-    }
+		private void genre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			/*
+			if (((Genre)genre.SelectedItem).IsSerialGenre)
+			{
+				MainInfo.MainWindow.FilmsMenu.UpdateVisualizer.OpenUpdateControl(filmControl);
+			}
+			*/
+		}
+	}
 }

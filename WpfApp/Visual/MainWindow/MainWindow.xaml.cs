d50223fd30660d7d492c61afded9368a88dd40e8
Andrey Le <sirkoandrew84@gmail.com>
2022-01-06 00:14:12 +0200

    Добавил сохранение файла через Ctrl+S

--- WpfApp/Visual/MainWindow/MainWindow.xaml.cs
+++ WpfApp/Visual/MainWindow/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace WpfApp.Visual.MainWindow
 		public MainWindow()
 		{
 			InitializeComponent();
+			MainInfo.TableCollection.TableSave += boolSaved;
+
+			InfoUnsaved = false;
 		}
 
 		public FilmsMenuControl FilmsMenu
@@ -45,7 +48,7 @@ namespace WpfApp.Visual.MainWindow
 		public Grid Grid { get { return grid; } }
 		public Grid Menus { get { return menus; } }
 
-		public Boolean InfoUnsaved { get; set; } = false;
+		public Boolean InfoUnsaved { get; set; }
 		private void boolSaved(object sender, EventArgs e)
 		{
 			InfoUnsaved = false;
@@ -70,7 +73,7 @@ namespace WpfApp.Visual.MainWindow
 
 		private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-			if (e.Key == Key.S && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+			if (e.Key == Key.S && (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)))
 			{
 				MainInfo.TableCollection.SaveTables();
 			}

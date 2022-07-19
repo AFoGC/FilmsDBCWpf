f41ec6661884b67fe000f78ec576294df869941a
Andrey Le <sirkoandrew84@gmail.com>
2022-07-19 17:56:42 +0300

    Долелал возможность лаунчера качать дополнительные файлы для основной программы

--- LauncherFDBC/Commands/ProgramUpdateCommand.cs
+++ LauncherFDBC/Commands/ProgramUpdateCommand.cs
@@ -57,8 +57,43 @@ namespace LauncherFDBC.Commands
                 string zipPath = Path.Combine(model.FdbcPath, "add.zip");
                 File.Delete(zipPath);
                 File.WriteAllBytes(zipPath, zipFile);
-                ZipFile.ExtractToDirectory(zipPath, model.FdbcPath);
+
+                using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+                {
+                    ExtractToDirectory(archive, model.FdbcPath, true);
+                }
+
                 File.Delete(zipPath);
+            }
+        }
+
+        public void ExtractToDirectory(ZipArchive archive, string destinationDirectoryName, bool overwrite)
+        {
+            if (!overwrite)
+            {
+                archive.ExtractToDirectory(destinationDirectoryName);
+                return;
+            }
+
+            DirectoryInfo di = Directory.CreateDirectory(destinationDirectoryName);
+            string destinationDirectoryFullPath = di.FullName;
+
+            foreach (ZipArchiveEntry file in archive.Entries)
+            {
+                string completeFileName = Path.GetFullPath(Path.Combine(destinationDirectoryFullPath, file.FullName));
+
+                if (!completeFileName.StartsWith(destinationDirectoryFullPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new IOException("Trying to extract file outside of destination directory. See this link for more info: https://snyk.io/research/zip-slip-vulnerability");
+                }
+
+                if (file.Name == "")
+                {// Assuming Empty for Directory
+                    Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                    continue;
+                }
+                Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                file.ExtractToFile(completeFileName, true);
             }
         }
 

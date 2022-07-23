bcac84004a6eced05b3312c462d92d6ee8362473
Andrey Le <sirkoandrew84@gmail.com>
2022-07-23 17:45:27 +0300

    Сделал отдельную папку для локализаций

--- WpfApp/LanguageHelper.cs
+++ WpfApp/LanguageHelper.cs
@@ -42,16 +42,16 @@ namespace WpfApp
                 ResourceDictionary dict = new ResourceDictionary();
                 if (Cultures.Contains(value))
                 {
-                    dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                    dict.Source = new Uri(String.Format("Resources/Localizations/lang.{0}.xaml", value.Name), UriKind.Relative);
                 }
                 else
                 {
-                    dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                    dict.Source = new Uri("Resources/Localizations/lang.xaml", UriKind.Relative);
                 }
 
                 //3. Находим старую ResourceDictionary и удаляем его и добавляем новую ResourceDictionary
                 ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
-                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/Localizations/lang.")
                                               select d).First();
                 if (oldDict != null)
                 {

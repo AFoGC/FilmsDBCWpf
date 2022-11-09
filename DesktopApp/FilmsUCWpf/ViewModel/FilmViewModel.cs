907959f873d0b388a9a0e4da9b51983b781e8932
AFoGC <sirkoandrew84@gmail.com>
2022-11-09 15:28:19 +0200

    Исправил работу с комментраиями (до этого они не отобрпжались)

--- DesktopApp/FilmsUCWpf/ViewModel/FilmViewModel.cs
+++ DesktopApp/FilmsUCWpf/ViewModel/FilmViewModel.cs
@@ -2,14 +2,10 @@
 using FilmsUCWpf.ViewModel.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Documents;
 using TL_Objects;
 using TL_Objects.Interfaces;
 using TL_Tables;

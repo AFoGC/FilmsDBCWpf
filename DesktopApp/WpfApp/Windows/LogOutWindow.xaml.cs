5fd1dfac70c2e9f1877a0fc6ca46537b775fc77a
AFoGC <sirkoandrew84@gmail.com>
2022-11-19 02:34:48 +0200

    Перенёс MoreInfoControl в Controls и LogOutWindow в Windows

--- DesktopApp/WpfApp/Windows/LogOutWindow.xaml.cs
+++ DesktopApp/WpfApp/Windows/LogOutWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfApp.Views
+namespace WpfApp.Windows
 {
     /// <summary>
     /// Логика взаимодействия для LogOutWindow.xaml

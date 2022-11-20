c4ed6b8d0f11c40982f4f31c5a09c89a5913e6be
AFoGC <sirkoandrew84@gmail.com>
2022-11-20 02:41:45 +0200

    Добавил SignIn View, Model и ViewModel

---
+++ DesktopApp/WpfApp/Models/SignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public class SignInModel
+    {
+        public void SignIn(string username, string password)
+        {
+
+        }
+    }
+}
